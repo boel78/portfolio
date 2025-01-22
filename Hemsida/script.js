eb9af3ba79d7aa5cfab9351d641f89c601b8cb64
boel78 <95147718+boel78@users.noreply.github.com>
2025-01-22 09:45:42 -0700

    dagens fix

--- Hemsida/script.js
+++ Hemsida/script.js
@@ -1,3 +1,5 @@
+const sections = document.querySelectorAll("section");
+const navLi = document.querySelectorAll("nav ul li");
 document.addEventListener("scroll", () => {
     const technologies = document.querySelectorAll(".technology");
   
@@ -6,10 +8,46 @@ document.addEventListener("scroll", () => {
       const isVisible = rect.top < window.innerHeight && rect.bottom >= 0;
   
       if (isVisible) {
-        tech.classList.add("visible"); // Lägg till klassen om den är synlig
+        tech.classList.add("visible");
       } else {
-        tech.classList.remove("visible"); // Ta bort klassen om den inte är synlig
+        tech.classList.remove("visible");
       }
     });
   });
   
+  let currentTheme = "light";
+  const themeSwitch = document.getElementById("themeSwitch");
+  const toggleTheme = () => {
+    document.body.classList.toggle("dark");
+    currentTheme = currentTheme === "light" ? "dark" : "light";
+    if(currentTheme === "light") {
+      themeSwitch.src = "svgs/sun-svgrepo-com.svg";
+    }
+    else{
+      themeSwitch.src = "svgs/moon.svg";
+    }
+  }
+
+  themeSwitch.addEventListener("click", toggleTheme);
+
+  window.addEventListener('scroll', () => {
+    let current = '';
+    sections.forEach( section => {
+      const sectionTop = section.offsetTop;
+      const sectionHeight = section.clientHeight;
+      if(scrollY >= (sectionTop - sectionHeight / 3)) {
+        current = section.getAttribute('id');
+        console.log(current);
+      }
+    });
+
+    navLi.forEach( li => {
+      li.classList.remove('active');
+      const liClass = li.className.replace('_nav', '');
+
+      if(liClass === current) {
+        li.classList.add('active');
+      }
+    });
+
+  });
