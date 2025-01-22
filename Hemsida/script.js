5a39b157b9b4a2c093684e95364063f45beba5b7
boel78 <95147718+boel78@users.noreply.github.com>
2025-01-22 10:30:01 -0700

    fix med design

--- Hemsida/script.js
+++ Hemsida/script.js
@@ -35,9 +35,8 @@ document.addEventListener("scroll", () => {
     sections.forEach( section => {
       const sectionTop = section.offsetTop;
       const sectionHeight = section.clientHeight;
-      if(scrollY >= (sectionTop - sectionHeight / 3)) {
+      if(scrollY >= (sectionTop - sectionHeight / 2)) {
         current = section.getAttribute('id');
-        console.log(current);
       }
     });
 
